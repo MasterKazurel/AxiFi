ce70a262692f4e4d558322e275bfacf4da237549
Sarah <aurezalia@gmail.com>
2017-11-28 09:37:24 -0700

    fixing transaction data

--- src/model/Testing.java
+++ src/model/Testing.java
@@ -4,21 +4,21 @@ public class Testing {
 
 	public static void fakeAdmin(CsAdmin admin) {
 		Profile trish = new Profile("Trish", "Duce", "csci323");
-		trish.getTransactions().add(new Transaction("10/01/17", "$100.00", "T-shirts"));
-		trish.getTransactions().add(new Transaction("10/23/17", "$23.00", "Cups"));
-		trish.getTransactions().add(new Transaction("11/02/17", "$1000.00", "Party hats"));
+		trish.getTransactions().add(new Transaction("10/01/17", "T-shirts", "$100.00"));
+		trish.getTransactions().add(new Transaction("10/23/17", "Cups", "$23.00"));
+		trish.getTransactions().add(new Transaction("11/02/17", "Party hats", "$1000.00"));
 		Profile michael = new Profile("Michael", "Cassens", "csci323");
-		michael.getTransactions().add(new Transaction("11/02/17", "$100.00", "Fireworks"));
-		michael.getTransactions().add(new Transaction("11/02/17", "$1000.00", "Bouncy castle"));
-		michael.getTransactions().add(new Transaction("11/17/17", "$20.00", "Pencils"));
+		michael.getTransactions().add(new Transaction("11/02/17", "Fireworks", "$100.00"));
+		michael.getTransactions().add(new Transaction("11/02/17", "Bouncy castle", "$1000.00"));
+		michael.getTransactions().add(new Transaction("11/17/17", "Pencils", "$20.00"));
 		Profile rob = new Profile("Rob", "Smith", "csci323");
-		rob.getTransactions().add(new Transaction("11/22/17", "$50.00", "Chairs"));
-		rob.getTransactions().add(new Transaction("11/17/17", "$20.00", "Ghost masks"));
-		rob.getTransactions().add(new Transaction("11/17/17", "$2000.00", "Lots of erasors"));
+		rob.getTransactions().add(new Transaction("11/22/17", "Chairs", "$50.00"));
+		rob.getTransactions().add(new Transaction("11/17/17", "Ghost masks", "$20.00"));
+		rob.getTransactions().add(new Transaction("11/17/17", "Lots of erasors", "$2000.00"));
 		Profile oliver = new Profile("Oliver", "Serang", "csci323");
-		oliver.getTransactions().add(new Transaction("11/17/17", "$200.00", "Penguin statue"));
-		oliver.getTransactions().add(new Transaction("11/17/17", "$20.00", "Binders"));
-		oliver.getTransactions().add(new Transaction("11/17/17", "$30.00", "Confetti"));
+		oliver.getTransactions().add(new Transaction("11/17/17", "Penguin statue", "$200.00"));
+		oliver.getTransactions().add(new Transaction("11/17/17", "Binders", "$20.00"));
+		oliver.getTransactions().add(new Transaction("11/17/17", "Confetti", "$30.00"));
 		admin.getUsers().add(trish);
 		admin.getUsers().add(michael);
 		admin.getUsers().add(rob);
